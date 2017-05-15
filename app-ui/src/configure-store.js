ed9c7c631c962bd8248cbc8266a13e50da0345b3
Mantas Domaševičius <mantas.domasevicius@funguide.lt>
2017-05-15 23:44:59 +0300

    Package UI with app

--- app-ui/src/configure-store.js
+++ app-ui/src/configure-store.js
@@ -9,32 +9,24 @@ import {browserHistory} from 'react-router';
 
 function configureStoreProd(initialState) {
     const middlewares = [
-        // Add other middleware on this line...
-
-        // thunk middleware can also accept an extra argument to be passed to each thunk action
-        // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+        reduxImmutableStateInvariant(),
+        routerMiddleware(browserHistory),
         promise(),
         thunk,
     ];
 
     return createStore(rootReducer, initialState, compose(
-        applyMiddleware(...middlewares),
-        routerMiddleware(browserHistory)
+        applyMiddleware(...middlewares)
         )
     );
 }
 
 function configureStoreDev(initialState) {
     const middlewares = [
-        // Add other middleware on this line...
-
-        // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
         reduxImmutableStateInvariant(),
         routerMiddleware(browserHistory),
         promise(),
         logger,
-        // thunk middleware can also accept an extra argument to be passed to each thunk action
-        // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
         thunk,
     ];
 
