43cfe063d8df5b4bd7ed126f1fd0ea317ec406d6
Mantas Domaševičius <mantas.domasevicius@funguide.lt>
2017-05-15 17:15:37 +0300

    Use custome page ad home page

--- app-ui/src/routes.js
+++ app-ui/src/routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {IndexRoute, Route} from 'react-router';
 
 import App from './app/App';
-import HomePage from './app/HomePage';
 import CustomerPage from './customer/CustomerPage';
+import CustomerDetailPage from './customer/CustomerDetailPage';
 import NotFoundPage from './app/NotFoundPage';
 
 export default (
     <Route path="/" component={App}>
-        <IndexRoute component={HomePage}/>
-        <Route path="/customers" component={CustomerPage}/>
+        <IndexRoute path="/" component={CustomerPage}/>
+        <Route path="/:customerId" components={CustomerDetailPage}/>
         <Route path="*" component={NotFoundPage}/>
     </Route>
 );
